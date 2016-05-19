f5f7b8ddd31358df5ffc2d208e9c7a52b9b0fc4b
Allen Holt <alholt@akamai.com>
2016-05-19 12:05:56 -0400

    Automated backup 2016-05-19

--- oh-my-zsh/.zshrc
+++ oh-my-zsh/.zshrc
@@ -1,63 +1,67 @@
-# Path to your oh-my-zsh configuration.
-ZSH=/Users/alholt/.oh-my-zsh
+# Path to your oh-my-zsh installation.
+export ZSH=/Users/alholt/.oh-my-zsh
 
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
-# ZSH_THEME="arrow"
-ZSH_THEME="clean"
-# ZSH_THEME="jispwoso"
 # time that oh-my-zsh is loaded.
-# ZSH_THEME="random"
+ZSH_THEME="simple"
 
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-
-# Set this to use case-sensitive completion
+# Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
 
-# Uncomment this to disable bi-weekly auto-update checks
+# Uncomment the following line to use hyphen-insensitive completion. Case
+# sensitive completion must be off. _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Uncomment the following line to disable bi-weekly auto-update checks.
 # DISABLE_AUTO_UPDATE="true"
 
-# Uncomment to change how often to auto-update? (in days)
+# Uncomment the following line to change how often to auto-update (in days).
 # export UPDATE_ZSH_DAYS=13
 
-# Uncomment following line if you want to disable colors in ls
+# Uncomment the following line to disable colors in ls.
 # DISABLE_LS_COLORS="true"
 
-# Uncomment following line if you want to disable autosetting terminal title.
+# Uncomment the following line to disable auto-setting terminal title.
 # DISABLE_AUTO_TITLE="true"
 
-# Uncomment following line if you want to disable command autocorrection
-# DISABLE_CORRECTION="true"
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
 
-# Uncomment following line if you want red dots to be displayed while waiting for completion
+# Uncomment the following line to display red dots whilst waiting for completion.
 # COMPLETION_WAITING_DOTS="true"
 
-# Uncomment following line if you want to disable marking untracked files under
-# VCS as dirty. This makes repository status check for large repositories much,
-# much faster.
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
 # DISABLE_UNTRACKED_FILES_DIRTY="true"
 
-# Uncomment following line if you want to the command execution time stamp shown
-# in the history command output.
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
 # The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
 # HIST_STAMPS="mm/dd/yyyy"
 
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-plugins=(git github rails osx ruby)
 # Example format: plugins=(rails git textmate ruby lighthouse)
-
-source $ZSH/oh-my-zsh.sh
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git)
 
 # User configuration
 
-export PATH="/usr/local/bin:/opt/local/bin:/opt/local/sbin:/Users/alholt/pear/bin:/Applications/Postgres.app/Contents/MacOS/bin:/bin:/sbin:/usr/bin:/usr/sbin:/Users/alholt/bin:/opt/local/bin"
+export PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/alholt/bin:/opt/local/bin"
 # export MANPATH="/usr/local/man:$MANPATH"
 
-# # Preferred editor for local and remote sessions
+source $ZSH/oh-my-zsh.sh
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
 # if [[ -n $SSH_CONNECTION ]]; then
 #   export EDITOR='vim'
 # else
@@ -70,42 +74,15 @@ export PATH="/usr/local/bin:/opt/local/bin:/opt/local/sbin:/Users/alholt/pear/bi
 # ssh
 # export SSH_KEY_PATH="~/.ssh/dsa_id"
 
-# bindkey -v
-# export KEYTIMEOUT=1
-# function zle-line-init zle-keymap-select {
-#   VIM_PROMPT="%{$fg_bold[yellow]%} [% VIM]% %{$reset_color%}"
-#   RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
-#   zle reset-prompt
-# }
-# zle -N zle-line-init
-# zle -N zle-keymap-select
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
 
-# # Use vim cli mode
-# bindkey '^P' up-history
-# bindkey '^N' down-history
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
-# # backspace and ^h working even after
-# # returning from command mode
-# bindkey '^?' backward-delete-char
-# bindkey '^h' backward-delete-char
-
-# # ctrl-w removed word backwards
-# bindkey '^w' backward-kill-word
-
-# # ctrl-r starts searching history backward
-# bindkey '^r' history-incremental-search-backward
-
-
-eval "$(rbenv init -)"
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-export P4CONFIG=.perforce
-export P4EDITOR=vi
-export P4USER=alholt
-export P4PORT="rsh:ssh -2 -q -a -x -l p4ssh perforce.akamai.com"
-export P4CLIENT=alholt_mac
-export P4DIFF="/Applications/p4merge.app/Contents/Resources/launchp4merge"
-export P4MERGE="/Applications/p4merge.app/Contents/Resources/launchp4merge"
-export P4RESOLVE="/Applications/p4merge.app/Contents/Resources/launchp4merge"
-
-# export RAILS_ENV="development"
+test -e ${HOME}/.iterm2_shell_integration.zsh && source ${HOME}/.iterm2_shell_integration.zsh
--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,44 +1,91 @@
-# Mac OSX aliases
-alias vi="/usr/local/Cellar/vim/7.4.488/bin/vim"
-alias vim="/usr/local/Cellar/vim/7.4.488/bin/vim"
+# TODO: Could use a script to automatically push this to git
+
+# Personal convenience scripts
+alias wow-backup='~/Dropbox/WoW/wow-backup.sh'
+
+# Mac OSX aliases - versions in Homebrew
+VIM_VERSION='7.4.979'
+alias g='/usr/local/Cellar/git/2.8.0/bin/git'
+alias vi="/usr/local/Cellar/vim/$VIM_VERSION/bin/vim"
+alias vim="/usr/local/Cellar/vim/$VIM_VERSION/bin/vim"
+
 alias vis="vi -S Session.vim"
 alias show_hidden="defaults write com.apple.Finder AppleShowAllFiles YES && killall Finder"
 
-# alias j="/usr/local/bin/autojump"
-[[ -s /Users/allen/.autojump/etc/profile.d/autojump.sh ]] && source /Users/allen/.autojump/etc/profile.d/autojump.sh
+alias j="/usr/local/bin/autojump"
+[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 
-alias g='/usr/bin/git'
 alias rcop='rubocop --format simple'
 alias guard='bundle exec guard'
-alias rs='bin/rails server'
-alias rc='bin/rails console'
-alias tc='RAILS_ENV=test bin/rails console'
-alias rtest='time bin/rake test'
-
-### HARMONIX
-# Python/Django aliases
-alias pm='python manage.py'
-alias ba=". bin/activate"
-alias sh+="pm shell_plus"
-alias rs+="pm runserver_plus 0.0.0.0:8000"
-alias db="pm dbshell"
-alias bantha="j bantha && ba"
-alias orion="cd ~/hmx/orion_www && ba"
-alias rb4eva="cd ~/hmx/rb4eva && ba"
-alias psql-dev='PGPASSWORD=RBq8utKDfBgu4b7q psql oriondev -Uoriondev -horion-dev.caf7vrrqkmdp.us-east-1.rds.amazonaws.com'
-
-### AKAMAI
+alias gz='bundle exec guard -g zeus -c'
+alias gs='bundle exec guard -g spring -c'
+alias bi='bundle install'
+alias rt='time z test'
+alias p4b='RAILS_ENV=test scripts/p4_bridge.rb'
+alias kill_zeus='pkill -9 "zeus.*" && pkill -9 "fsevents-wrapper$"'
 
 alias st3pack='cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User'
-
 alias md='mkdir -pv'
 
-# autoload -U compinit && compinit
-# Autocomplete for 'g' as well
-# complete -o default -o nospace -F _git g
+# Allows HEAD^ to work in zsh
+setopt NO_NOMATCH
+
+### AKAMAI
+alias agent-ext='/usr/bin/ssh-add $HOME/.ssh/external/2016-03-30;'
+alias agent-int='/usr/bin/ssh-add $HOME/.ssh/internal/2016-03-30; /usr/bin/ssh-add $HOME/.ssh/govops_lab_key_v2'
+alias agent-old='/usr/bin/ssh-add $HOME/.ssh/internal/2015-12-14; /usr/bin/ssh-add $HOME/.ssh/external/2015-12-14'
+alias agent-wf='/usr/bin/ssh-add $HOME/.ssh/wf/id_rsa'
+alias agent-bb='/usr/bin/ssh-add $HOME/.ssh/bitbucket/2016-05-19'
+
+# alias sshgen_external='ssh-keygen -t rsa -b 2048 -C "`whoami`-external-`date +%Y-%m-%d`" -f ~/.ssh/external/`date +%Y-%m-%d`'
+# alias sshgen_internal='ssh-keygen -t rsa -b 2048 -C "`whoami`-internal-`date +%Y-%m-%d`" -f ~/.ssh/internal/`date +%Y-%m-%d`'
+# alias sshgen_deployed='ssh-keygen -t rsa -b 2048 -C "`whoami`-deployed-`date +%Y-%m-%d`" -f ~/.ssh/deployed/`date +%Y-%m-%d`'
+# alias sshgen_nipr='ssh-keygen -t rsa -b 2048 -C "`whoami`-nipr-`date +%Y-%m-%d`" -f ~/.ssh/nipr/`date +%Y-%m-%d`'
+# alias sshgen_sipr='ssh-keygen -t rsa -b 2048 -C "`whoami`-sipr-`date +%Y-%m-%d`" -f ~/.ssh/sipr/`date +%Y-%m-%d`'
+
+function akakeygen {
+  ssh-keygen -t rsa -b 2048 -C "`whoami`-$1-`date +%Y-%m-%d`" -f ~/.ssh/$1/`date +%Y-%m-%d`
+}
+
+# Run akakeyconnect in one window, then run akakeyconfirm in another
+function akakeyconnect {
+  old_date="$1"
+  for key_type in internal external; do
+    ssh -i ~/.ssh/${key_type}/${old_date} kv${key_type}@ssh-keyrotation.akamai.com;
+  done
+}
+
+function akakeyconfirm {
+  todays_date=`date +"%Y-%m-%d"`
+  for key_type in internal external; do
+    ssh-keygen -l -f ~/.ssh/${key_type}/${todays_date}.pub;
+  done
+}
+
+alias mp="cd ~/code/mcdn-portal/"
 
 alias push='git push origin master'
 alias pull='git pull origin master'
+alias merge_develop='g fetch origin develop && g merge FETCH_HEAD'
+function go_get {
+  echo 'Fetching repo '$1
+  git fetch origin $1
+  git co $1
+}
+
+function gdv { git co develop }
+function gfe { git co "feature/MCDNPORTAL-$1" }
+function gbf { git co "bugfix/MCDNPORTAL-$1" }
+function gff {
+  branch="feature/MCDNPORTAL-$1"
+  git fetch origin "$branch"
+  git co "$branch"
+}
+function gfb {
+  branch="bugfix/MCDNPORTAL-$1"
+  git fetch origin "$branch"
+  git co "$branch"
+}
 
 alias pushprofile2="scp ~/.profile.template lab2:~/.profile"
 function pushprofile {
@@ -83,24 +130,8 @@ function p4e { p4 edit $1 && vi $1 }
 function p4r { p4 revert $1 }
 function p4d { p4 diff $1 }
 
-alias agent-ext='/usr/bin/ssh-add $HOME/.ssh/external/2014-10-08;'
-alias agent-int='/usr/bin/ssh-add $HOME/.ssh/internal/2014-10-08; /usr/bin/ssh-add $HOME/.ssh/govops/govops_lab_key_v2'
-alias agent-old='/usr/bin/ssh-add $HOME/.ssh/internal/2014-06-17; /usr/bin/ssh-add $HOME/.ssh/external/2014-06-17'
-alias agent-wf='/usr/bin/ssh-add $HOME/.ssh/wf/id_rsa'
 
-# alias sshgen_external='ssh-keygen -t rsa -b 2048 -C "`whoami`-external-`date +%Y-%m-%d`" -f ~/.ssh/external/`date +%Y-%m-%d`'
-# alias sshgen_internal='ssh-keygen -t rsa -b 2048 -C "`whoami`-internal-`date +%Y-%m-%d`" -f ~/.ssh/internal/`date +%Y-%m-%d`'
-# alias sshgen_deployed='ssh-keygen -t rsa -b 2048 -C "`whoami`-deployed-`date +%Y-%m-%d`" -f ~/.ssh/deployed/`date +%Y-%m-%d`'
-# alias sshgen_nipr='ssh-keygen -t rsa -b 2048 -C "`whoami`-nipr-`date +%Y-%m-%d`" -f ~/.ssh/nipr/`date +%Y-%m-%d`'
-# alias sshgen_sipr='ssh-keygen -t rsa -b 2048 -C "`whoami`-sipr-`date +%Y-%m-%d`" -f ~/.ssh/sipr/`date +%Y-%m-%d`'
-
-function akakeygen {
-  ssh-keygen -t rsa -b 2048 -C "`whoami`-$1-`date +%Y-%m-%d`" -f ~/.ssh/$1/`date +%Y-%m-%d`
-}
-
-alias startredis='redis-server /usr/local/etc/redis.conf'
-alias redis-dev='redis-cli -h orion-dev.dq2my3.0001.use1.cache.amazonaws.com'
-alias redis-prod='redis-cli -h rb-prod.dq2my3.ng.0001.use1.cache.amazonaws.com'
+alias psql='/Applications/Postgres.app/Contents/Versions/9.4/bin/psql'
 alias repost='/Applications/Postgres.app/Contents/MacOS/Postgres restart -D ~/Library/Application\ Support/Postgres/var/ -m fast -s'
 alias pdb='psql portal'
 alias rake='bundle exec rake'
@@ -117,51 +148,45 @@ alias vdb='psql vulcan'
 alias v='cd ~/git/vulcan'
 
 export ZCUSTOM="~/.oh-my-zsh/custom/"
-alias eza="vi ~/.oh-my-zsh/custom/aliases.zsh"
+alias eza="atom ~/.oh-my-zsh/custom/aliases.zsh"
 # alias sza="source ~/.oh-my-zsh/custom/aliases.zsh"
 alias sza="source ~/.zshrc"
-
-function p () {
-  echo 'Switching to mcdn-portal '$1
-  case $1 in
-    rel)  cd ~/p4/releases;;
-    3.5)  cd ~/git/mcdn-portal-3.5/;;
-    3.4)  cd ~/git/mcdn-portal-3.4/;;
-    3.3)  cd ~/git/mcdn-portal-3.3/;;
-    "")   cd ~/git;;
-    *)    cd ~/git;;
-  esac
-}
-
-function pp4 () {
-  echo 'Switching to mcdn-portal '$1
-  case $1 in
-    rel)  cd ~/p4/releases;;
-    3.5)  cd ~/p4/mcdn-portal-3.5/akamai/mcdn-portal;;
-    3.4)  cd ~/p4/mcdn-portal-3.4/akamai/mcdn-portal;;
-    3.3)  cd ~/p4/mcdn-portal-3.3/akamai/mcdn-portal;;
-    3.2)  cd ~/p4/mcdn-portal-3.2/akamai/mcdn-portal;;
-  esac
-}
 
 function killit () {
   # Kills any process that matches a regexp passed to it
   ps aux | ack -v "ack" | ack "$@" | awk '{print $2}' | xargs kill -9
 }
 
-# Set architecture flags
-export ARCHFLAGS="-arch x86_64"
-export PATH=/usr/local/bin:$PATH
-# pip should only run if there is a virtualenv currently activated
-export PIP_REQUIRE_VIRTUALENV=true
-# cache pip-installed packages to avoid re-downloading
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-gpip(){
-    PIP_REQUIRE_VIRTUALENV="" pip "$@"
-}
-
 export P4CONFIG=".perforce"
-export P4USER="allen.holt"
+export P4USER="alholt"
 export P4HOST="perforce:1666"
+export P4PORT="rsh:/usr/local/bin/crackpipe ssh -2 -a -c blowfish -l p4ssh -q -x perforce.akamai.com /bin/true"
 export P4EDITOR="vi"
 export P4IGNORE=".pyc"
+
+
+### HARMONIX - archived
+# Python/Django aliases
+# alias pm='python manage.py'
+# alias ba=". bin/activate"
+# alias sh+="pm shell_plus"
+# alias rs+="pm runserver_plus 0.0.0.0:8000"
+# alias db="pm dbshell"
+# alias bantha="j bantha && ba"
+# alias orion="cd ~/hmx/orion_www && ba"
+# alias rb4eva="cd ~/hmx/rb4eva && ba"
+# alias psql-dev='PGPASSWORD=RBq8utKDfBgu4b7q psql oriondev -Uoriondev -horion-dev.caf7vrrqkmdp.us-east-1.rds.amazonaws.com'
+# alias startredis='redis-server /usr/local/etc/redis.conf'
+# alias redis-dev='redis-cli -h orion-dev.dq2my3.0001.use1.cache.amazonaws.com'
+# alias redis-prod='redis-cli -h rb-prod.dq2my3.ng.0001.use1.cache.amazonaws.com'
+#
+# # Set architecture flags
+# export ARCHFLAGS="-arch x86_64"
+# export PATH=/usr/local/bin:$PATH
+# # pip should only run if there is a virtualenv currently activated
+# export PIP_REQUIRE_VIRTUALENV=true
+# # cache pip-installed packages to avoid re-downloading
+# export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+# gpip(){
+#     PIP_REQUIRE_VIRTUALENV="" pip "$@"
+# }
