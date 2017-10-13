26d258c5af9fc95ec3506028bd1e96083face949
Allison Holt <hey@allenholt.com>
2017-10-13 10:13:25 -0400

    Automated backup 2017-10-13

--- oh-my-zsh/custom/todotxt.zsh
+++ oh-my-zsh/custom/todotxt.zsh
@@ -1,16 +1,15 @@
 # == Todo.txt Stuff
 alias t="clear && todo.sh"
 alias today="clear && todo.sh ls @today"
-alias tak="clear && todo.sh ls @akamai"
+alias tak="clear && todo.sh ls @aka"
 
 taa () {
-  branch_name="$(git symbolic-ref --short HEAD)"
-  todo.sh add "[@akamai][$branch_name] $1"
+  todo.sh add "[@aka][$(git_branch_id)] $1"
 }
 
 tj () {
   clear
   branch_name="$(git symbolic-ref --short HEAD)"
-  todo.sh ls $branch_name
+  todo.sh ls git_branch_id
 }
 
